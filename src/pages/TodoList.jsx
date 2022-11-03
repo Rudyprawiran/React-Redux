719a57589eb7d6100a43e9af1fca8dc04134782b
Rudyprawiran <116810591+Rudyprawiran@users.noreply.github.com>
2022-11-03 13:18:29 +0700

    Add files via upload

---
+++ src/pages/TodoList.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Form from "../components/form/Form";
+import Header from "../components/header/Header";
+import Layout from "../components/layout/Layout";
+import List from "../components/list/List";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Learning React",
+      body: "Let's learn the Advanced React.",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "Learning React",
+      body: "Let's learn the basics of React.",
+      isDone: true,
+    },
+  ]);
+
+  return (
+    <Layout>
+      <Header />
+      <Form setTodos={setTodos} todos={todos} />
+      <List todos={todos} setTodos={setTodos} />
+    </Layout>
+  );
+};
+
+export default TodoList;
